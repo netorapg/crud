44e0b4245e8d0bf93f19b66eda7a0faead077ab2
netorapg <renatoplatz11@outlook.com>
2024-02-22 15:26:29 -0300

    trabalhando no modelo de usuarios

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,10 @@
+import '../models/user.dart';
+
+const DUMMY_USERS = {
+    '1': User(
+      id: '1',
+      name: 'Mario',
+      email: 'mario@alunos.com.br',
+      avatarUrl: 'https://cdn.pixabay.com/photo/2017/02/23/13/05/avatar-2092113_960_720.png',
+    ),
+};
